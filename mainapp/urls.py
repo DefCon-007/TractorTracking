325b30cb60857688a4f2b17f5f5c2e8747a06aa9
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-10-05 14:22:50 +0530

    Adds clear option and date

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("add/", views.addNewCoordinate),
-    path('getcoordinate/', views.sendCoordinates, name="getcoordinate")
+    path('getcoordinate/', views.sendCoordinates, name="getcoordinate"),
+    path('clear', views.clear, name="clear"),
 ]
